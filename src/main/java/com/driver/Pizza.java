7a5ff6e0fd536ab2db8fcddb6f4e79427af09f05
Navneet <navneetmui@gmail.com>
2023-09-01 12:40:14 +0530

    reverted back to price

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,13 +5,12 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-    private int total;
+    //private int total;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
         price = isVeg ? 300 : 400;
-        total += price;
         bill = "Base Price Of The Pizza: " + price +"\n";
         System.out.println();
     }
@@ -23,9 +22,8 @@ public class Pizza {
     public void addExtraCheese(){
         // your code goes here
         if(!bill.contains("Extra Cheese Added: ")){
-            int cheesePrice = 80;
-            total += cheesePrice;
-            bill += "Extra Cheese Added: " +cheesePrice+"\n";
+            price += 80;
+            bill += "Extra Cheese Added: 80\n";
         }
 
     }
@@ -34,7 +32,7 @@ public class Pizza {
         // your code goes here
         if(!bill.contains("Extra Toppings Added: ")){
             int toppingPrice = isVeg? 70 : 120;
-            total += toppingPrice;
+            price += toppingPrice;
             bill += "Extra Toppings Added: "+toppingPrice+"\n";
         }
 
@@ -43,15 +41,14 @@ public class Pizza {
     public void addTakeaway(){
         // your code goes here
         if(!bill.contains("Paperbag Added: ")){
-            int takeout = 20;
-            total += takeout;
-            bill += "Paperbag Added: " + takeout +"\n";
+            price += 20;
+            bill += "Paperbag Added: 20\n";
         }
     }
 
     public String getBill(){
         // your code goes here
-        bill += "Total Price: " + total+"\n";
+        bill += "Total Price: " + price +"\n";
         return this.bill;
     }
 }
