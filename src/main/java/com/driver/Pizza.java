b42b374f73acc029cd03bb824306945b01525fe3
Navneet <navneetmui@gmail.com>
2023-09-01 11:59:37 +0530

    first try

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,6 +9,8 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        price = isVeg ? 300 : 400;
+        bill += "Base Price Of The Pizza: " + price +"/n";
     }
 
     public int getPrice(){
@@ -17,18 +19,32 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!bill.contains("Extra Cheese Added:")){
+            price += 80;
+            bill += "Extra Cheese Added: 80 /n";
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!bill.contains("Extra Toppings Added:")){
+            int toppingPrice = isVeg ? 70:120;
+            price += toppingPrice;
+            bill += "Extra Toppings Added: "+ toppingPrice +"/n";
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if(!bill.contains("Paperbag Added: ")){
+            price += 20;
+            bill += "Paperbag Added: 20 /n";
+        }
     }
 
     public String getBill(){
         // your code goes here
+        bill += "Total Price: " + price;
         return this.bill;
     }
 }
