d24a8be35ab258f86f4296058d3339824e4abcf6
Navneet <navneetmui@gmail.com>
2023-09-01 13:15:08 +0530

    \n

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -48,7 +48,7 @@ public class Pizza {
 
     public String getBill(){
         // your code goes here
-        bill += "Total Price: " + price;
+        bill += "Total Price: " + price + "\n";
         return this.bill;
     }
 }
